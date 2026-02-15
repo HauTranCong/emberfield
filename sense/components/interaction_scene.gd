f717378ef20c111e269168312e7d323a2071db3b
Tuong Tran Kien <tuongtran1106@gmail.com>
2026-02-16 04:14:03 +0700

    [T1P-3-7]: Implement integration interact shared component

---
+++ sense/components/interaction_scene.gd
@@ -0,0 +1,24 @@
+extends Area2D
+class_name InteractionArea
+
+@export var action_name: String = "interact"
+@export var global_position_area: Vector2
+@export var interact: Callable = func() -> void:
+	pass
+
+func _ready() -> void:
+	# Interactable area: Layer INTERACTABLE, Mask: PLAYER
+	collision_layer = CollisionLayers.Layer.INTERACTABLE
+	collision_mask = CollisionLayers.Layer.PLAYER
+	interact = _on_interact
+
+func _on_body_entered(body: Node2D) -> void:	
+	InteractionManager.register_area(self)
+	print("InteractionArea ready at global position: ", body.global_position)
+	global_position_area = body.global_position
+
+func _on_body_exited(body: Node2D) -> void:
+	InteractionManager.unregister_area(self)
+
+func _on_interact() -> void:
+	print("Player is interacting with ", action_name)
